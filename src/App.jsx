d032cd534738784c92aab73544d374db274be391
Pinto Manuel <pintoydc@gmail.com>
2022-03-07 19:11:16 +0200

    Bes commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import { Routes, Route, } from "react-router-dom";
 import Dashboard from './components/Dashboard';
+import PageError from './pages/PageError';
 
 
 const App = () => {
@@ -9,7 +10,8 @@ const App = () => {
   return (
     <div className="App" >
       <Routes>
-        <Route path="/" element={<Dashboard />}/>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="*" element={<PageError />} />
      </Routes>
       
     </div>
---
+++ src/pages/PageError.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styledError from '../pages/PageError.module.css';
+
+const PageError = () => {
+    return (
+        <div>
+            <h2>Page Not Found</h2>
+        </div>
+    );
+};
+
+export default PageError;
