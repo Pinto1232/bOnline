a6bb098f9e19b56e2f3d0852735f38a1fffca2e1
Pinto Manuel <pintoydc@gmail.com>
2022-03-07 19:00:13 +0200

    more to commit

--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div> 
-            <h2>Page not found</h2>
+            <h2>Page not found!</h2>
         </div>
     );
 };
